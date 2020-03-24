467735c8e13c31a1749d17bf3ea57c824c62a2d1
Glitch (chaudha4-metric-imp-converter) <none>
2020-03-24 20:40:02 +0000

    🏁⚽️ Checkpoint ./controllers/convertHandler.js:11501875/1390

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -51,15 +51,38 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
+    
+    let result;
+    switch(initUnit) {
+      case "gal":
+        result = initNum * galToL;
+        break;
+      case "l":
+        result = initNum / galToL;;
+        break;
+      case "mi":
+        result = initNum * miToKm;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break; 
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;      
+      case "kg":
+        result = initNum / lbsToKg;
+        break;            
+      default:
+        // nothing to do;
+    }    
     
     return result;
+
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
     
-    return result;
+    return initNum + " " + initUnit + " converts to " + returnNum + " " + returnUnit;
   };
   
 }
